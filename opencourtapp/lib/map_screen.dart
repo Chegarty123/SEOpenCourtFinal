c933e35cc099e1ce67d2d93b26bcf262bf05e43d
tylerheckel2 <tylerheckel2@gmail.com>
2024-04-13 19:46:09 -0400

    Worked on Map Screen
    Implemented Map API commands

--- opencourtapp/lib/map_screen.dart
+++ opencourtapp/lib/map_screen.dart
@@ -1,14 +1,37 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapScreen extends StatefulWidget {
-  
+  const MapScreen({Key ? key }): super(key:key);
+
   @override
-  _MapScreenState createState() => _MapScreenState();
+  State<MapScreen> createState() => _MapScreenState();
 }
 
 class _MapScreenState extends State<MapScreen> {
+
+  final Completer<GoogleMapController> _controller = Completer();
+
+  static const CameraPosition _initialPosition = CameraPosition(
+    target: LatLng(40.03603365942587, -75.34103877887395),
+    zoom: 14,
+    
+    );
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: GoogleMap(
+          initialCameraPosition: _initialPosition,
+          onMapCreated: (GoogleMapController controller) 
+          {
+            _controller.complete(controller);
+          },
+        ),
+      ),
+    );
   }
 }
