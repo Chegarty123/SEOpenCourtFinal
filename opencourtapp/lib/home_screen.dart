5fedeb71df2a8ecbbd9b90ee6bccded19b1ea668
Chegarty123 <135582879+Chegarty123@users.noreply.github.com>
2024-04-20 17:14:58 -0400

    Redone HomeScreen

--- opencourtapp/lib/home_screen.dart
+++ opencourtapp/lib/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:opencourtapp/color_utils.dart';
-import 'package:opencourtapp/map_screen.dart';
 import 'package:opencourtapp/signin_screen.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,54 +10,145 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late PageController _pageController;
+  int totalPage = 3;
+
+  void _onScroll(){
+  }
+
+
+  @override
+  void initState() {
+    _pageController
+      = PageController(
+          initialPage: 0,
+      )..addListener(_onScroll);
+
+      super.initState();
+  }  
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: _pageController,
+        children: <Widget>[
+          makePage(
+            page: 1,
+            image: 'assets/images/west.jpeg',
+            title: "West Campus",
+            description: 'Contains a selection of oudoor courts, as well as an indoor facility that has a total of 6 different hoops.'  
+          ),
+          makePage(
+            page: 2,
+            image: 'assets/images/main.jpeg',
+            title: 'Main Campus',
+            description: "The area of campus with the most amount of choices. Contains a total of 7 different full courts that make up the Driscoll outdoor courts, as well as the indoor courts of Alumni Hall and Nevin Fieldhouse."  
+          ),
+          makePage(
+            page: 3,
+            image: 'assets/images/south.jpeg',
+            title: 'South Campus',
+            description: 'South Campus is made up of one full outdoor court that contains 2 different hoops.'   
+          ),
+        ],       
+      )
+    );
+  }
+
+  Widget makePage({image, title, description, page}){
     return Container(
       decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            hexStringToColor("0f1056"),
-            hexStringToColor("151269"),
-            hexStringToColor("81b1ce"),
-          ],
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-        ),
+        image: DecorationImage(
+          image: AssetImage(image),
+          fit: BoxFit.cover
+          )
       ),
-      child: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.only(top: 16),
-              child: ElevatedButton(
-                child: Text("Logout"),
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => SignInScreen()));
-                },
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.bottomRight,
+            stops: [0.3, 0.9],
+            colors:[
+              Colors.black.withOpacity(0.9),
+              Colors.black.withOpacity(0.2),
+            ]
+          )
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 40,
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 16),
-              child: ElevatedButton(
-                child: Text("Map Page"),
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => MapScreen()));
-                },
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.baseline,
+                textBaseline: TextBaseline.alphabetic,
+                children: <Widget>[
+                  Text(page.toString(), style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),),
+                  Text('/'+ totalPage.toString(), style: TextStyle(color: Colors.white, fontSize: 15),)
+                ],
               ),
-            ),
-            Expanded(
-              child: Center(
-                child: Image.asset(
-                  "assets/images/logo.png",
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(title, style: TextStyle(color: Colors.white, fontSize: 50, fontWeight: FontWeight.bold),),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.only(right: 3), 
+                          child: Icon(Icons.star, color: Colors.yellow, size: 15),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(right: 3), 
+                          child: Icon(Icons.star, color: Colors.yellow, size: 15),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(right: 3), 
+                          child: Icon(Icons.star, color: Colors.yellow, size: 15),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(right: 3), 
+                          child: Icon(Icons.star, color: Colors.yellow, size: 15),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(right: 5), 
+                          child: Icon(Icons.star, color: Colors.grey, size: 15),
+                        ),
+                        Text('4.0', style: TextStyle(color: Colors.white70),),
+                        Text('(2300)', style: TextStyle(color: Colors.white38, fontSize: 12),),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 50),
+                      child: Text(description, style: TextStyle(color: Colors.white.withOpacity(.7), height: 1.9, fontSize: 15),),
+                    ),
+                    SizedBox(height: 20,),
+                    Text('READ MORE', style: TextStyle(color: Colors.white),),
+                    SizedBox(height: 30,),
+                  ],
                 ),
-              ),
-            ),
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+// Remove the incomplete code block
